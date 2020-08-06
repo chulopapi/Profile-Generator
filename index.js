d6c10a9fea2b7f6d9463bc0b62b7d0d4895f08b2
chulopapi <marlon.guandique@gmail.com>
2020-08-05 23:18:43 -0700

    getting index file

---
+++ index.js
@@ -0,0 +1,7 @@
+// Written by Marlon Guandique
+
+const fs = require('fs')
+const inquirer = require('inquirer')
+const intern = req('./lib/intern')
+const engineer = req('./lib/engineer')
+const manager = req('./lib/manager')
