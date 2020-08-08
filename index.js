11b83c51b9959e1a9fea550b5c8e4b26d265623c
chulopapi <marlon.guandique@gmail.com>
2020-08-08 13:10:00 -0700

    adding intern-employee's input

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const questions = {
         },
         {
             type: "input",
-            name: "id",
+            name: "employeeID",
             message: "Team's manager ID number:",
             validate: validateId
         },
@@ -34,6 +34,58 @@ const questions = {
             validate: validateOfficeNumber
         },
 
+    ],
+    engineerQuestions: [
+        {
+            type: 'input',
+            name: 'name',
+            message: "Engineer's name:",
+            validate: validateName
+        },
+        {
+            type: 'input',
+            name: 'employeeID',
+            message: "Engineer's ID number:",
+            validate: validateNumber
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Engineer's email address:",
+            validate: validateEmail
+        },
+        {
+            type: "input",
+            name: "gitHub",
+            message: "Engineer's GitHub username:",
+            validate: validateEmail
+        },
+    ],
+    internQuestions: [
+        {
+            type: 'input',
+            name: 'name',
+            message: "Intern's name:",
+            validate: validateName
+        },
+        {
+            type: 'input',
+            name: 'employeeID',
+            message: "Intern's ID number:",
+            validate: validateNumber
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Intern's email address:",
+            validate: validateEmail
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "Intern's current school name:",
+            validate: validateEmail
+        },
     ],
 }
 
