ea0e623e67bd2f744892ad9c544e71b89af484cb
chulopapi <marlon.guandique@gmail.com>
2020-08-08 12:36:00 -0700

    adding manager's input

--- index.js
+++ index.js
@@ -5,3 +5,35 @@ const inquirer = require('inquirer')
 const intern = req('./lib/intern')
 const engineer = req('./lib/engineer')
 const manager = req('./lib/manager')
+
+
+const questions = {
+    managerQuestions: [
+        {
+            type: "input",
+            name: "name",
+            message: "Team's manager name:",
+            validate: validateName
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Team's manager ID number:",
+            validate: validateId
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Team's manager email address:",
+            validate: validateEmail
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "Team's manager office number:",
+            validate: validateOfficeNumber
+        },
+
+    ],
+}
+
